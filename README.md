0a543d602d1a84717db80d0cb34a514421aa698b
GLCVLL <g.ioele96@outlook.it>
2023-06-04 10:57:53 +0200

    added mark-up text

---
+++ README.md
@@ -0,0 +1,17 @@
+*** START ***
+
+- recupero l'elemento **prev** dal DOM
+- recupero l'elemento **next** dal DOM
+- recupero l'elemento **gallery** dal DOM
+- preparo le immagini 
+- preparo HTML per le immagini
+- per ogni url dell'immagine, creo un tag <img>
+- inserisco immagine nel DOM
+- preparo la gestione dell'index
+- do la classe .active alla prima immagine
+- *** QUANDO *** clicco sul pulsante prev 
+  - **SE** mi trovo sulla prima immagine devo andare all'ultima
+  - **ALTRIMENTI** Torno all'immagine precedente.
+- *** QUANDO *** clicco sul pulsante next
+  - **SE** mi trovo sull'ultima immagine devo andare alla prima
+  - **ALTRIMENTI** vado all'immagine successiva   
--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
 console.log('JS OK');
+
+/*************  OPERAZIONI INIZIALI ***************/
+
+// RECUPERO GLI ELEMENTI PREV E NEXT DAL DOM
+
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+console.log(prevButton, nextButton);
+
+// RECUPERO L'ELEMENTO GALLERY DAL DOM
+
+const galleryElements = document.querySelector('.gallery');
+console.log(galleryElements);
+
+// PREPARIAMO LE IMMAGINI
+
+const sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+
+// PREPARO HTML PER LE IMMAGINI
+
+let imageElements = '';
+
+// PER OGNI URL DELL'IMMAGINE CREO UN TAG <IMG>
+
+for(let i = 0; i < sources.length; i++){
+    imageElements += `<img src= "${sources[i]}">`;
+}
+
+// INSERISCO LE IMMAGINI NEL DOM
+
+galleryElements.innerHTML = imageElements;
+
+// RECUPERO TUTTE LE IMMAGINI
+
+const images = document.querySelectorAll('#carousel img');
+
+// PREPARO LA GESTIONE DELL'INDEX
+
+let currentIndex = 0;
+
+// DO LA CLASSE ACTIVE ALLA PRIMA IMMAGINE
+
+images[currentIndex].classList.add('active');
+
+
+/*********** LOGICA EFFETTIVA ************/
+
+
+
